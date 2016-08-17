2be56931beb675e4bb962b9ce1b672e1ca37ef2b
Trey Pendragon <tpendragon@princeton.edu>
2016-08-17 15:54:19 -0700

    Ignore singleton classes for descendants.
    Strange issue that would come up in test suites under Ruby 2.3.

--- spec/unit/iiif/service_spec.rb
+++ spec/unit/iiif/service_spec.rb
@@ -1,9 +1,27 @@
 describe IIIF::Service do
 
   describe 'self#get_descendant_class_by_jld_type' do
+    before do
+      class DummyClass < IIIF::Service
+        TYPE = "sc:Collection"
+        def self.singleton_class?
+          true
+        end
+      end
+    end
+    after do
+      Object.send(:remove_const, :DummyClass)
+    end
     it 'gets the right class' do
       klass = described_class.get_descendant_class_by_jld_type('sc:Canvas')
       expect(klass).to be IIIF::Presentation::Canvas
+    end
+    context "when there are singleton classes which are returned" do
+      it "gets the right class" do
+        allow(IIIF::Service).to receive(:descendants).and_return([DummyClass, IIIF::Presentation::Collection])
+        klass = described_class.get_descendant_class_by_jld_type('sc:Collection')
+        expect(klass).to be IIIF::Presentation::Collection
+      end
     end
   end
 
