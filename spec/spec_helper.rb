6926ff3c8007f03558bccdff6584bc2d5b24bcc1
Trey Pendragon <tpendragon@princeton.edu>
2016-08-18 10:56:33 -0700

    Run local coverage too

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,10 @@ VCR.configure do |c|
   c.configure_rspec_metadata!
 end
 
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start do
   add_filter 'spec/'
 end
