7bbe7ddfd5b40df75af04981819db8d418840675
Naomi Dushay <ndushay@stanford.edu>
2017-06-27 16:17:09 -0700

    v3 has AnnotationPage but no AnnotationList

--- spec/unit/iiif/v3/presentation/annotation_page_spec.rb
+++ spec/unit/iiif/v3/presentation/annotation_page_spec.rb
@@ -1,4 +1,4 @@
-describe IIIF::V3::Presentation::AnnotationList do
+describe IIIF::V3::Presentation::AnnotationPage do
 
   describe "#{described_class}.define_methods_for_array_only_keys" do
     it_behaves_like 'it has the appropriate methods for array-only keys v3'
