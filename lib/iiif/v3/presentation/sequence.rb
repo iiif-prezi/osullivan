5ce1fb43815d84bf8066583c79ddb54bd8077ebf
Naomi Dushay <ndushay@stanford.edu>
2017-07-17 16:54:08 -0700

    v3 sequence: improve specificity, validation and tests

--- lib/iiif/v3/presentation/sequence.rb
+++ lib/iiif/v3/presentation/sequence.rb
@@ -3,10 +3,14 @@ module IIIF
     module Presentation
       class Sequence < IIIF::V3::AbstractResource
 
-        TYPE = 'Sequence'
+        TYPE = 'Sequence'.freeze
 
         def required_keys
           super + %w{ items }
+        end
+
+        def prohibited_keys
+          super + CONTENT_RESOURCE_PROPERTIES + PAGING_PROPERTIES + %w{ nav_date content_annotations }
         end
 
         def legal_viewing_hint_values
@@ -20,8 +24,20 @@ module IIIF
 
         def validate
           super
-          # TODO: Must be at least one canvas
-          # TODO: All members of canvases must be a kind of Canvas
+
+          unless self['items'].size >= 1
+            m = 'The items list must have at least one entry (and it must be a IIIF::V3::Presentation::Canvas)'
+            raise IIIF::V3::Presentation::MissingRequiredKeyError, m
+          end
+
+          unless self['items'].all? { |entry| entry.instance_of?(IIIF::V3::Presentation::Canvas) }
+            m = 'All entries in the items list must be a IIIF::V3::Presentation::Canvas'
+            raise IIIF::V3::Presentation::IllegalValueError, m
+          end
+
+          # TODO: startCanvas: A link from a Sequence or Range to a Canvas that is contained within it
+
+          # TODO: All external Sequences must have a dereference-able http(s) URI
         end
       end
     end
