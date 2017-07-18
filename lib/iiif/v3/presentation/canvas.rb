0f5eafb871a535df37fd205a9052f6da88b5b14b
Naomi Dushay <ndushay@stanford.edu>
2017-07-17 23:41:53 -0700

    v3 canvas: improve specificity, validation and tests (WIP)

--- lib/iiif/v3/presentation/canvas.rb
+++ lib/iiif/v3/presentation/canvas.rb
@@ -3,25 +3,22 @@ module IIIF
     module Presentation
       class Canvas < IIIF::V3::AbstractResource
 
-        # TODO (?) a simple 'Image Canvas' constructor.
-
-        TYPE = 'Canvas'
+        TYPE = 'Canvas'.freeze
 
         def required_keys
           super + %w{ id label }
+        end
+
+        def prohibited_keys
+          super + PAGING_PROPERTIES + %w{ viewing_direction format nav_date start_canvas content_annotations }
         end
 
         def array_only_keys
           super + %w{ content }
         end
 
-        # TODO: test and validate
-        def int_only_keys
-          super + %w{ width height }
-        end
-
         def legal_viewing_hint_values
-          super + %w{ non-paged }
+          super + %w{ paged continuous non-paged facing-pages auto-advance }
         end
 
         def initialize(hsh={})
@@ -31,7 +28,36 @@ module IIIF
 
         def validate
           super
-          # TODO: all members of content are of type AnnotationPage
+
+          id_uri = URI.parse(self['id'])
+          unless self['id'] =~ /^https?:/ && id_uri.fragment.nil?
+            err_msg = "id must be an http(s) URI without a fragment for #{self.class}"
+            raise IIIF::V3::Presentation::IllegalValueError, err_msg
+          end
+
+          content = self['content']
+          if content && content.any?
+            unless content.all? { |entry| entry.instance_of?(IIIF::V3::Presentation::AnnotationPage) }
+              err_msg = 'All entries in the content list must be a IIIF::V3::Presentation::AnnotationPage'
+              raise IIIF::V3::Presentation::IllegalValueError, err_msg
+            end
+          end
+
+          # "A canvas MUST have exactly one width and one height, or exactly one duration.
+          # It may have width, height and duration.""
+          height = self['height']
+          width = self['width']
+          extent_err_msg = "#{self.class} must have (a height and a width) and/or a duration"
+          if (!!height ^ !!width) # this is an exclusive or: forces height and width to boolean
+            raise IIIF::V3::Presentation::IllegalValueError, extent_err_msg
+          end
+          duration = self['duration']
+          unless (height && width) || duration
+            raise IIIF::V3::Presentation::IllegalValueError, extent_err_msg
+          end
+
+          # TODO: Content must not be associated with space or time outside of the Canvas’s dimensions,
+          # such as at coordinates below 0,0, greater than the height or width, before 0 seconds, or after the duration.
         end
       end
     end
