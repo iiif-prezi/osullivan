21708b234044c41af4248ef659bd31f65e0b73b1
Naomi Dushay <ndushay@stanford.edu>
2017-07-20 12:27:27 -0700

    v3 resource: improve specificity, validation and tests

--- lib/iiif/v3/presentation/resource.rb
+++ lib/iiif/v3/presentation/resource.rb
@@ -1,20 +1,29 @@
 module IIIF
   module V3
     module Presentation
+      # class for generic content resource
       class Resource < IIIF::V3::AbstractResource
 
         def required_keys
-          %w{ id }
+          super + %w{ id }
         end
 
-        def string_only_keys
-          super + %w{ format }
+        def prohibited_keys
+          super + PAGING_PROPERTIES + %w{ nav_date viewing_direction start_canvas content_annotations}
         end
 
-        def numeric_only_keys
-          super + %w{ duration }
+        def uri_only_keys
+          super + %w{ id }
         end
 
+        def validate
+          super
+
+          unless self['id'] =~ /^https?:/
+            err_msg = "id must be an http(s) URI for #{self.class}"
+            raise IIIF::V3::Presentation::IllegalValueError, err_msg
+          end
+        end
       end
     end
   end
