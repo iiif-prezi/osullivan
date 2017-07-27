43ae937943302a29804f9ee2b78d7a14f8a4ace0
Naomi Dushay <ndushay@stanford.edu>
2017-07-26 18:19:27 -0700

    v3 sequence: allow 'canvases' as well as 'items' as UniversalViewer only works with the former

--- lib/iiif/v3/presentation/sequence.rb
+++ lib/iiif/v3/presentation/sequence.rb
@@ -5,12 +5,20 @@ module IIIF
 
         TYPE = 'Sequence'.freeze
 
-        def required_keys
-          super + %w{ items }
-        end
+        # NOTE:  relaxing requirement for items as Universal Viewer currently only accepts canvases
+        #  see https://github.com/sul-dlss/osullivan/issues/27, sul-dlss/purl/issues/167
+        # def required_keys
+        #   super + %w{ items }
+        # end
 
         def prohibited_keys
           super + CONTENT_RESOURCE_PROPERTIES + PAGING_PROPERTIES + %w{ nav_date content_annotations }
+        end
+
+        # NOTE: allowing 'items' or 'canvases' as Universal Viewer currently only accepts canvases
+        #  see https://github.com/sul-dlss/osullivan/issues/27, sul-dlss/purl/issues/167
+        def array_only_keys
+          super + %w{ canvases }
         end
 
         def legal_viewing_hint_values
@@ -25,19 +33,32 @@ module IIIF
         def validate
           super
 
-          unless self['items'].size >= 1
-            m = 'The items list must have at least one entry (and it must be a IIIF::V3::Presentation::Canvas)'
+          # Canvas object list
+          # NOTE: allowing 'items' or 'canvases' as Universal Viewer currently only accepts canvases
+          #  see https://github.com/sul-dlss/osullivan/issues/27, sul-dlss/purl/issues/167
+          unless (self['items'] && self['items'].any?) ||
+              (self['canvases'] && self['canvases'].any?)
+            m = 'The (items or canvases) list must have at least one entry (and it must be a IIIF::V3::Presentation::Canvas)'
             raise IIIF::V3::Presentation::MissingRequiredKeyError, m
           end
-
-          unless self['items'].all? { |entry| entry.instance_of?(IIIF::V3::Presentation::Canvas) }
-            m = 'All entries in the items list must be a IIIF::V3::Presentation::Canvas'
-            raise IIIF::V3::Presentation::IllegalValueError, m
-          end
+          validate_canvas_list(self['items']) if self['items']
+          validate_canvas_list(self['canvases']) if self['canvases']
 
           # TODO: startCanvas: A link from a Sequence or Range to a Canvas that is contained within it
 
           # TODO: All external Sequences must have a dereference-able http(s) URI
+        end
+
+        def validate_canvas_list(canvas_array)
+          unless canvas_array.size >= 1
+            m = 'The (items or canvases) list must have at least one entry (and it must be a IIIF::V3::Presentation::Canvas)'
+            raise IIIF::V3::Presentation::MissingRequiredKeyError, m
+          end
+
+          unless canvas_array.all? { |entry| entry.instance_of?(IIIF::V3::Presentation::Canvas) }
+            m = 'All entries in the (items or canvases) list must be a IIIF::V3::Presentation::Canvas'
+            raise IIIF::V3::Presentation::IllegalValueError, m
+          end
         end
       end
     end
