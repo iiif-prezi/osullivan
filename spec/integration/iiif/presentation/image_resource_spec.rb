89ecaa62c07ea7b977e99dd9cb107699c4194acb
Ben W. Brumfield <benwbrum@gmail.com>
2017-10-03 12:16:01 -0500

    Fixed stale URIs -- Princeton has move to HTTPS, etc

--- spec/integration/iiif/presentation/image_resource_spec.rb
+++ spec/integration/iiif/presentation/image_resource_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 describe IIIF::Presentation::ImageResource do
   vcr_options = {
     cassette_name: 'pul_loris_cassette',
@@ -7,15 +8,14 @@ describe IIIF::Presentation::ImageResource do
 
   describe 'self#create_image_api_image_resource', vcr: vcr_options do
 
-    let(:image_server) { 'http://libimages.princeton.edu/loris2' }
-
+    let(:image_server) { 'https://libimages.princeton.edu/loris' }
     let(:valid_service_id) {
-      id = 'pudl0001%2F4612422%2F00000001.jp2'
+      id = 'pudl0001/4612422/00000001.jp2'
       "#{image_server}/#{id}"
     }
 
     let(:invalid_service_id) {
-      id = 'xxxx%2F4612422%2F00000001.jp2'
+      id = 'xxxx/4612422/00000001.jp2'
       "#{image_server}/#{id}"
     }
 
@@ -31,25 +31,25 @@ describe IIIF::Presentation::ImageResource do
         resource = described_class.create_image_api_image_resource(opts)
         # expect(resource['@context']).to eq 'http://iiif.io/api/presentation/2/context.json'
         # @context is only added when we call to_json...
-        expect(resource['@id']).to eq 'http://libimages.princeton.edu/loris2/pudl0001%2F4612422%2F00000001.jp2/full/!200,200/0/default.jpg'
+        expect(resource['@id']).to eq 'https://libimages.princeton.edu/loris/pudl0001/4612422/00000001.jp2/full/!200,200/0/default.jpg'
         expect(resource['@type']).to eq 'dctypes:Image'
         expect(resource.format).to eq "image/jpeg"
         expect(resource.width).to eq 3047
         expect(resource.height).to eq 7200
         expect(resource.service['@context']).to eq 'http://iiif.io/api/image/2/context.json'
-        expect(resource.service['@id']).to eq 'http://libimages.princeton.edu/loris2/pudl0001%2F4612422%2F00000001.jp2'
+        expect(resource.service['@id']).to eq 'https://libimages.princeton.edu/loris/pudl0001/4612422/00000001.jp2'
         expect(resource.service['profile']).to eq 'http://iiif.io/api/image/2/level2.json'
       end
       it 'copies over all teh infos (when copy_info is true)' do
         opts = { service_id: valid_service_id, copy_info: true }
         resource = described_class.create_image_api_image_resource(opts)
-        expect(resource['@id']).to eq 'http://libimages.princeton.edu/loris2/pudl0001%2F4612422%2F00000001.jp2/full/!200,200/0/default.jpg'
+        expect(resource['@id']).to eq 'https://libimages.princeton.edu/loris/pudl0001/4612422/00000001.jp2/full/!200,200/0/default.jpg'
         expect(resource['@type']).to eq 'dctypes:Image'
         expect(resource.format).to eq "image/jpeg"
         expect(resource.width).to eq 3047
         expect(resource.height).to eq 7200
         expect(resource.service['@context']).to eq 'http://iiif.io/api/image/2/context.json'
-        expect(resource.service['@id']).to eq 'http://libimages.princeton.edu/loris2/pudl0001%2F4612422%2F00000001.jp2'
+        expect(resource.service['@id']).to eq 'https://libimages.princeton.edu/loris/pudl0001%2F4612422%2F00000001.jp2'  # I do not know why this would be partially escaped, but I am not Princeton
         expect(resource.service['profile']).to eq [
           'http://iiif.io/api/image/2/level2.json',
           {
