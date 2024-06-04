fd85ece5239226a28afae87ac750c243539f9b16
Hugh A. Cayless <philomousos@gmail.com>
2024-06-04 12:42:51 -0400

    Make sure metadata keys are treated as strings.

--- lib/iiif/v3/abstract_resource.rb
+++ lib/iiif/v3/abstract_resource.rb
@@ -156,7 +156,7 @@ module IIIF
             raise IIIF::V3::Presentation::IllegalValueError, m
           end
           self['metadata'].each do |entry|
-            md_keys = entry.keys
+            md_keys = entry.keys.map(&:to_s)
             unless md_keys.size == 2 && md_keys.include?('label') && md_keys.include?('value')
               m = "metadata members must be a Hash of keys 'label' and 'value', found #{md_keys}"
               raise IIIF::V3::Presentation::IllegalValueError, m
